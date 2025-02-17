8c2a5ec7715f53cc3e7247a9bafc5b788da92897
laura <lg8104@gmail.com>
2025-02-17 16:48:48 -0700

    Updating index page, stylesheet, and script file

--- script.js
+++ script.js
@@ -1,1 +1,2 @@
-const
+const wireframe = document.querySelector(".wire");
+alert("Wireframing is like drawing blueprints for you house. You can plan logo wirefplacement, along with images, headings, and paragraphs.);
