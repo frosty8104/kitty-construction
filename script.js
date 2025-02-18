cee78a7f18ee221c1ac701fac91520aff959210a
laura <lg8104@gmail.com>
2025-02-17 22:28:26 -0700

    Working on adding content to kitty construction page, styling to stylesheet, and variables and alerts to script file

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const wireframe = document.querySelector(".wire");
-alert("Wireframing is like drawing blueprints for you house. You can plan logo wirefplacement, along with images, headings, and paragraphs.);
+const colour = document.querySelector(".colour");
+const typography = document.querySelector(".type");
+const whitespace = document.querySelector(".space");
+alert("Wireframing is like drawing blueprints for you house. You can plan logo wirefplacement, along with images, headings, and paragraphs.");
+alert("Colour theory tells you what colours invite or connect to what emotions. This will help you pick a colour scheme of two, maximum three colours to use for your website and visual brand. Colour theory resembles choosing paint colours for your house.");
+alert("Typography is the pairing of fonts for the best visual presentation of your website, business card, and other promotional pieces that help your company stand out. Typography is like decorating your house, whether it be wallpaper or otherwise.");
+alert("Whitespace gives elements breathing space. Whitespace resembles space between walls for living room, bathroom, bedroom, and any other room where you have to put furniture and appliances and also space between said furniture and appliances so there's room for you to move between them.");
